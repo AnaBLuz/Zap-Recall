10e1f1a794984d3bb0cfbb8baed7767c6b03bd5e
Bia Luz <anabluz08@gmail.com>
2023-03-06 15:33:35 -0300

    Muda a cor da pergunta depois de concluida

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import seta_play from '../assets/img/seta_play.png'
+import seta_virar from '../assets/img/seta_virar.png'
+import icone_erro from '../assets/img/icone_erro.png'
+import icone_quase from '../assets/img/icone_quase.png'
+import icone_certo from '../assets/img/icone_certo.png'
 
 function Cards({iniciar}){
 
@@ -21,6 +25,7 @@ function Cards({iniciar}){
         {cards.map((c,index) =>
                     <Card 
                     question = {c.question}
+                    answer = {c.answer}
                     index = {index}
                     
                     />)}
@@ -30,18 +35,60 @@ function Cards({iniciar}){
 }
 export default Cards;
 
-function Card({question, index}){
+function Card({question,answer, index}){
 
     const [abrirPergunta, setabrirPergunta] = useState(false);
+    const [abrirResposta, setabrirResposta] = useState(false);
+    const [icone, setIcone] = useState(seta_play);
+    const [errou,setErrou] = useState(false);
+    const [quaseErrou, setQuaseErrou] = useState(false);
+    const [acertou, setAcertou] = useState(false);
 
     function mostrarCard() {
         setabrirPergunta(true);
 
         }
+    function virarCard(){
+        setabrirResposta(true);
+
+    }
+    function naoLembrou(){
+        setabrirPergunta(false);
+        setabrirResposta(false);
+        setIcone(icone_erro);
+        setErrou(true); 
+    }
+    function quasenaoLembrou(){
+        setabrirPergunta(false);
+        setabrirResposta(false);
+        setIcone(icone_quase);
+        setQuaseErrou(true);
+
+    }
+
+    function lembrou(){
+        setabrirPergunta(false);
+        setabrirResposta(false);
+        setIcone(icone_certo);
+        setAcertou(true);
+        
+    }
       
     return(abrirPergunta? 
-        <CardRecall data-test="flashcard">{question}</CardRecall>:
-        <CardRecall data-test="flashcard">Pergunta {index +1} <img  data-test="play-btn" src={seta_play} alt="seta_play" onClick={mostrarCard}/></CardRecall>);
+        (abrirResposta? 
+        <CardRecall abrirPergunta={abrirPergunta}>{answer}
+        <Lembretes><CardVermelho onClick={naoLembrou}>Não lembrei</CardVermelho>
+        <CardLaranja onClick={quasenaoLembrou}>Quase não lembrei</CardLaranja>
+        <CardVerde onClick={lembrou} >Zap!</CardVerde>
+        </Lembretes>
+        </CardRecall>:
+        <CardRecall abrirPergunta={abrirPergunta} data-test="flashcard"> 
+        <span data-test="flashcard-text">{question}</span> 
+        <img src={seta_virar} alt="seta_virar" onClick={virarCard}/></CardRecall>):
+        <CardRecall abrirPergunta={abrirPergunta} errou={errou} quaseErrou = {quaseErrou} acertou={acertou} data-test="flashcard"> 
+        <span data-test="flashcard-text"> Pergunta {index +1}</span> 
+        <img  data-test="play-btn" src={icone} alt={icone} onClick={mostrarCard}/>
+        </CardRecall>);
 }
 
 
@@ -58,9 +105,9 @@ overflow-y:scroll;
 
 const CardRecall = styled.div`
 width: 300px;
-height: 45px;
+height: ${props => props.abrirPergunta ? '131px':'45px'};
 margin-bottom: 25px;
-background: #FFFFFF;
+background:${props => props.abrirPergunta ? '#FFFFD5':'#FFFFFF'};
 box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
 border-radius: 5px;
 font-family: 'Recursive';
@@ -68,9 +115,65 @@ font-style: normal;
 font-weight: 700;
 font-size: 16px;
 line-height: 19px;
-color: #333333;
+color:${props => props.errou ? '#FF3030':(props.quaseErrou ? '#FF922E':props.acertou ? '#2FBE34':'#333333')};
+text-decoration-line:${props => props.errou ? 'line-through':'none'};
 padding: 15px;
 display:flex;
-align-items:center;
-justify-content:space-between;
+flex-direction:${props => props.abrirPergunta ? 'column':'row'};
+align-items:${props => props.abrirPergunta ? 'none':'center'};
+justify-content:${props => props.abrirPergunta ? 'none':'space-between'};
+img {
+    width:${props => props.abrirPergunta ? '30px':'20'};
+    height:20px;
+    margin-left: ${props => props.abrirPergunta ? '254px':'0'};
+    margin-top:${props => props.abrirPergunta ? '100px':'0'};
+    
+}
+
 `
+const Lembretes = styled.div`
+display:flex;
+justify-content:space-between;
+margin-top: 50px;
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 15px;
+line-height: 14px;
+color: #FFFFFF;
+
+`
+
+
+const CardVermelho = styled.div`
+width: 85px;
+height: 37px;
+background: #FF3030;
+border-radius: 5px;
+text-align: center;
+display: flex;
+justify-content:center;
+align-items:center;
+flex-wrap:wrap;
+`
+const CardLaranja = styled.div`
+width: 85px;
+height: 37px;
+background: #FF922E;
+border-radius: 5px;
+text-align: center;
+display: flex;
+justify-content:center;
+align-items:center;
+flex-wrap:wrap;`
+
+const CardVerde = styled.div`
+width: 85px;
+height: 37px;
+background: #2FBE34;
+border-radius: 5px;
+text-align: center;
+display: flex;
+justify-content:center;
+align-items:center;
+flex-wrap:wrap;`
